1b5536b1909f58e1535a84a4ae0a380f657e9c9e
Paul-DS <paul.david.sivelle@gmail.com>
2019-01-04 06:12:20 +0100

    Create the first version of the library

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # gigz-tracking
 Tracking library for Gigz
+
+## Installation
+
+```
+npm install --save gigz-tracking
+```
+
+## Setup
+
+In your app initialization
+
+```
+import gigz from 'gigz-tracking';
+
+// Use the following if you have a token based access
+gigz.initToken("YOUR-TOKEN");
+
+// OR
+
+// Use the following if you have a proxy based access
+await gigz.initProxy("YOUR-GET-TOKEN-URL", "YOUR-CHECK-TOKEN-URL");
+```
+
+Then you can track any event with the following code
+
+```
+gigz.track("My event type", { event_gigz_id: "XXX" });
+```
+
+Use the following when a user successfully authentifies on your website
+
+```
+gigz.engage("USER_ID", "FIRSTNAME", "EMAIL", "USER_SUBSCRIPTION_TIME");
+```
+
+And the following when the user signs out
+
+```
+gigz.reset();
+```
---
+++ src/index.js
@@ -0,0 +1,127 @@
+const uuidv4 = require('uuid/v4');
+
+const apiUrl = 'https://gigz.simbals.com';
+let distinct_id = null;
+let token = null;
+let proxy = null;
+
+module.exports = {
+  initToken: function(newToken) {
+    this._init();
+    token = newToken;
+  },
+  initProxy: async function(getTokenUrl, checkTokenUrl) {
+    this._init();
+
+    proxy = {
+      getToken: getTokenUrl,
+      checkToken: checkTokenUrl,
+      token: null
+    };
+
+    // Get token from cookies
+    proxyToken = this._getCookie("gigz-tracking-token");
+
+    // TODO: Request token
+    if (!proxyToken || !(await this._checkProxyToken(proxyToken))) {
+      proxyToken = await this._getProxyToken();
+      this._setCookie("gigz-tracking-token", proxyToken);
+    }
+
+    proxy.token = proxyToken;
+  },
+  _init: function() {
+    // Get distinct id from cookies
+    distinct_id = this._getCookie("gigz-tracking-distinctid");
+
+    if (!distinct_id) {
+      distinct_id = uuidv4();
+      this._setCookie("gigz-tracking-distinctid", distinct_id);
+    }
+  },
+  _getCookie(name) {
+    var selectedCookie = document.cookie.split(';').map(c => c.split('=')).filter(c => c[0].trim() == name);
+
+    return selectedCookie.length ? selectedCookie[0][1].trim() : null;
+  },
+  _setCookie(name, value) {
+    document.cookie = `${name}=${value}`;
+  },
+  _getProxyToken() {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      
+      xhr.open('GET', proxy.getToken);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+          if (xhr.response.status == 1) {
+            resolve(xhr.response.result.session_key);
+          }
+          else {
+            reject();
+          }
+        }
+      }
+
+      xhr.send();
+    });
+  },
+  _checkProxyToken(proxyToken) {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+
+      xhr.open('GET', proxy.checkToken.replace("{0}", proxyToken));
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+          resolve(xhr.response.status == 1);
+        }
+      }
+
+      xhr.send();
+    });
+  },
+  track: function(eventName, parameters) {
+    var xhr = new XMLHttpRequest();
+    
+    if (token == null && (proxy == null || proxy.token == null)) {
+      throw new Error('Invalid API token');  
+    }
+
+    xhr.open('POST', `${apiUrl}/log/${token || proxy.token}/track`);
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    var body = {
+      action: eventName,
+      distinct_id: distinct_id,
+      agent: navigator.userAgent,
+      properties: parameters != null ? parameters : {}
+    };
+
+    xhr.send(JSON.stringify(body));
+  },
+  engage: function(userId, firstName, email, creationTime) {
+    var xhr = new XMLHttpRequest();
+
+    if (token == null && (proxy == null || proxy.token == null)) {
+      throw new Error('Invalid API token');  
+    }
+
+    xhr.open('POST', `${apiUrl}/log/${token || proxy.token}/engage`);
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    var body = {
+      distinct_id: distinct_id,
+      user_id: userId,
+      name: firstName,
+      email: email,
+      created: creationTime / 1000
+    };
+
+    xhr.send(JSON.stringify(body));
+  },
+  reset: function() {
+    distinct_id = uuidv4();
+  }
+};
